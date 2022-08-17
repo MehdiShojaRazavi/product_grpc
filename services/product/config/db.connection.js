b85df3f3940ce019939af0e9a4a62e18576724e1
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-17 18:46:43 +0300

    add db config to service app

---
+++ services/product/config/db.connection.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+module.exports = mongoose.connect("mongodb://localhost:27017/grpc-nodejs")
+.then(() => console.log("Connected to db!"))
+.catch(err => console.log(err.message)) 
