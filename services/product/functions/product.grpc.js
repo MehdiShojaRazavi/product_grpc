6a38c7503d9153eb5e8468eb4c316656dc444725
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-18 11:50:07 +0300

    add getProduct fn in product.grpc file in service app

--- services/product/functions/product.grpc.js
+++ services/product/functions/product.grpc.js
@@ -1,12 +1,27 @@
 const {ProductModel} = require("./../models/product.model")
 async function listProduct(call, callback) {
     try {
+        console.log('is running: listProduct(call, callback)')
         const products = await ProductModel.find({})
+        console.log(products)
         callback(null, {products})
     } catch (error) {
         callback(error, null)
     }
 }
+async function getProduct(call, callback) {
+    try {
+        console.log('is running: getProduct(call, callback)')
+        const {id} = call.request;
+        console.log('id:', id);
+        const product = await ProductModel.findOne({id});
+        console.log(product);
+        callback(null, product)
+    } catch (error) {
+        callback(error, null)
+    }
+}
 module.exports = {
-  listProduct
+  listProduct,
+  getProduct
 }
