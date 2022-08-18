bf33ef9f9b9a7ab78888aaff56e61bf6dd6abf8d
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-18 12:17:00 +0300

    add route all product to swaggerUI >> client app

---
+++ clients/src/routes/product/swagger.js
@@ -0,0 +1,22 @@
+/**
+ * @swagger
+ * tags: admin-panel
+ * description: product page section
+ */
+
+/**
+ * @swagger
+ *  /product/list:
+ *    get:
+ *      tags: [admin-panel]
+ *      summary: get all product
+ *      responses:
+ *        201:
+ *          description: success
+ *        400:
+ *          description: data not found
+ *        404:
+ *          description: route not found.
+ *        500:
+ *          description: Internal Server Error
+ */
