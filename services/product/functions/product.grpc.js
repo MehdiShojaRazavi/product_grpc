fd74c0b55e84dca31a099e4c6a8a592a09396e08
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-19 22:32:06 +0300

    cleaning debug codes

--- services/product/functions/product.grpc.js
+++ services/product/functions/product.grpc.js
@@ -1,9 +1,7 @@
 const {ProductModel} = require("./../models/product.model")
 async function listProduct(call, callback) {
     try {
-        console.log('is running: listProduct(call, callback)')
         const products = await ProductModel.find({})
-        console.log(products)
         callback(null, {products})
     } catch (error) {
         callback(error, null)
@@ -11,11 +9,8 @@ async function listProduct(call, callback) {
 }
 async function getProduct(call, callback) {
     try {
-        console.log('is running: getProduct(call, callback)')
         const {id} = call.request;
-        console.log('id:', id);
         const product = await ProductModel.findOne({id});
-        console.log(product);
         callback(null, product)
     } catch (error) {
         callback(error, null)
