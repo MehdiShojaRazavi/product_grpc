0a17c0782197bd31ac53b758f6506f599b95c93f
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-19 23:09:28 +0300

    add deleteProduct fn to controller > service app

--- services/product/functions/product.grpc.js
+++ services/product/functions/product.grpc.js
@@ -16,7 +16,18 @@ async function getProduct(call, callback) {
         callback(error, null)
     }
 }
+async function deleteProduct(call, callback) {
+    try {
+        const {id} = call.request;
+        const result = await ProductModel.deleteOne({id});
+        if(result.deletedCount > 0) return callback(null, {status: "deleted"})
+        return callback({message: "cannot deleted"}, null)
+    } catch (error) {
+        callback(error, null)
+    }
+}
 module.exports = {
   listProduct,
-  getProduct
+  getProduct,
+  deleteProduct
 }
