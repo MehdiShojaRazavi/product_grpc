28b4cbbbf615a99e83a33aa0ba3a62dc5f197a99
MehdiShojaRazavi <MehdiShojaRazavi@gmail.com>
2022-08-19 22:39:09 +0300

    add swagger, route getProduct

--- clients/src/routes/product/swagger.js
+++ clients/src/routes/product/swagger.js
@@ -20,3 +20,27 @@
  *        500:
  *          description: Internal Server Error
  */
+
+/**
+ * @swagger
+ *  /product/{id}:
+ *    get:
+ *      tags: [admin-panel]
+ *      summary: get product by id
+ *      description: get product by id
+ *      parameters:
+ *        -  in: path
+ *           name: id
+ *           type: string
+ *           description: Enter id
+ *           example: 4
+ *      responses:
+ *        201:
+ *          description: success
+ *        400:
+ *          description: data not found
+ *        404:
+ *          description: route not found
+ *        500:
+ *          description: Internal Server Error
+ */
